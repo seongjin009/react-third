c69794616b0b0f0888b791ce517be88c961ab7cc
seongjin009 <iseongjin009@gmail.com>
2023-10-05 14:14:40 +0900

    팝업창 활성화 시 스크롤 비활성화

--- src/components/common/modal/Modal.jsx
+++ src/components/common/modal/Modal.jsx
@@ -1,24 +1,19 @@
 import './Modal.scss';
-import { forwardRef, useImperativeHandle, useState } from 'react';
-//forwardRef : forwaedRef(( props,ref)=>{})
-//forwardRef안쪽에서 ref로 연결한 요소를 역으로 부모컴포넌트에 전달 가능 (자식 커뮤ㅗ넌트의 jsx요소를 부모로 전달)
-//useImperativeHandle : 자식요소의 특정 객체값을 역으로 부모 컴포넌트에 전달
+import { useEffect } from 'react';
 
-const Modal = forwardRef(({ children, setIsModal }, ref) => {
-	const [Open, setOpen] = useState(false);
-	useImperativeHandle(ref, () => {
-		return { open: () => setOpen(true) };
-	});
+const Modal = ({ children, setIsModal }) => {
+	useEffect(() => {
+		document.body.style.overflow = 'hidden';
+
+		return () => {
+			document.body.style.overflow = 'auto';
+		};
+	}, []);
 	return (
-		<>
-			{Open && (
-				<aside className='modal'>
-					<div className='con'>{children}</div>
-					<span onClick={() => setOpen(false)}>close</span>
-				</aside>
-			)}
-		</>
+		<aside className='modal'>
+			<div className='con'>{children}</div>
+			<span onClick={() => setIsModal(false)}>close</span>
+		</aside>
 	);
-});
-
+};
 export default Modal;
--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -1,24 +1,12 @@
-import Layout from '../../common/layout/Layout';
 import Modal from '../../common/modal/Modal';
 import './Youtube.scss';
-import { useEffect, useState, useRef } from 'react';
-/*
-리엑트는 단방향 데이터 바인딩
--부모에서 자식으로 데이터 전달가능하지만 자식에서 부모로는 데이터를 전달 불가
--props전달, children 전달
-
-리엑트에서 자식 컴포넌트에서는 직접적으로 부모 ㅂ컴포넌트의 state값 변경이 불가
--해결방법 부모의 state변경함수를 자식 컴포넌트로 전달
--자식 컴포넌트에서는 전달받은 state변경함수로 간접적으로 부모 state값 변경가능
-
-useRef로 jsx참조 객체에 담을 수 있음
-컴포넌트를 useRef를 통한 참조객체 감는것이 불가
- */
+import Layout from '../../common/layout/Layout';
+import { useEffect, useState } from 'react';
 
 export default function Youtube() {
-	const refEl = useRef(null);
 	const [Youtube, setYoutube] = useState([]);
 	const [IsModal, setIsModal] = useState(false);
+	const [Index, setIndex] = useState(0);
 
 	const fetchYoutube = () => {
 		const api_key = process.env.REACT_APP_YOUTUBE_API;
@@ -26,7 +14,6 @@ export default function Youtube() {
 		const pid = `PLJ0_dUpwgnHGNKFN5G2r6rsMf51JmkB7M`;
 		const num = 5;
 		const resultURL = `${baseURL}?key=${api_key}&part=snippet&playlistId=${pid}&maxResult=${num}`;
-
 		fetch(resultURL)
 			.then((data) => data.json())
 			.then((json) => {
@@ -34,27 +21,39 @@ export default function Youtube() {
 				setYoutube(json.items);
 			});
 	};
-
 	useEffect(() => {
 		fetchYoutube();
 	}, []);
+
 	return (
 		<>
-			<Modal setIsModal={setIsModal} ref={refEl}></Modal>
-
 			<Layout title={'Youtube'}>
 				{Youtube.map((data, idx) => {
 					return (
 						<article key={idx}>
-							<h2 onClick={() => console.log(refEl)}>{data.snippet.title}</h2>
-							<p ref={refEl}>{data.snippet.description}</p>
-							<div className='pic' onClick={() => refEl.current.open()}>
+							<h2>{data.snippet.title}</h2>
+							<p>{data.snippet.description}</p>
+							<div
+								className='pic'
+								onClick={() => {
+									setIndex(idx);
+									setIsModal(true);
+								}}
+							>
 								<img src={data.snippet.thumbnails.standard.url} alt={data.title} />
 							</div>
 						</article>
 					);
 				})}
 			</Layout>
+			{IsModal && (
+				<Modal setIsModal={setIsModal}>
+					<iframe
+						src={`https://www.youtube.com/embed/${Youtube[Index].snippet.resourceId.videoId}`}
+						title='youtube'
+					></iframe>
+				</Modal>
+			)}
 		</>
 	);
 }
