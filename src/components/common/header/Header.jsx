dfd7029f4947bec757e3e253cc84ca11082fcaf4
seongjin009 <iseongjin009@gmail.com>
2023-10-05 09:19:58 +0900

    초기화

---
+++ src/components/common/header/Header.jsx
@@ -0,0 +1,39 @@
+import { Link, NavLink } from 'react-router-dom';
+
+export default function Header({ isMain }) {
+	return (
+		<header>
+			<h1>
+				<Link to='/'>LOGO</Link>
+			</h1>
+
+			<ul>
+				<li>
+					<NavLink to='/department' activeClassName='active'>
+						Department
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/gallery' activeClassName='active'>
+						Gallery
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/youtube' activeClassName='active'>
+						Youtube
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/members' activeClassName='active'>
+						Members
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/contact' activeClassName='active'>
+						Contact
+					</NavLink>
+				</li>
+			</ul>
+		</header>
+	);
+}
