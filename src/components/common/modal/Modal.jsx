d4716d90cfd65484b43073195b589e86ff909b85
seongjin009 <iseongjin009@gmail.com>
2023-10-05 13:30:38 +0900

    자식 컴포넌트에 state변경 함수를역으로 부모에게 전달

--- src/components/common/modal/Modal.jsx
+++ src/components/common/modal/Modal.jsx
@@ -1,12 +1,24 @@
 import './Modal.scss';
+import { forwardRef, useImperativeHandle, useState } from 'react';
+//forwardRef : forwaedRef(( props,ref)=>{})
+//forwardRef안쪽에서 ref로 연결한 요소를 역으로 부모컴포넌트에 전달 가능 (자식 커뮤ㅗ넌트의 jsx요소를 부모로 전달)
+//useImperativeHandle : 자식요소의 특정 객체값을 역으로 부모 컴포넌트에 전달
 
-const Modal = ({ children, setIsModal }) => {
+const Modal = forwardRef(({ children, setIsModal }, ref) => {
+	const [Open, setOpen] = useState(false);
+	useImperativeHandle(ref, () => {
+		return { open: () => setOpen(true) };
+	});
 	return (
-		<aside className='modal'>
-			<div className='con'>{children}</div>
-			<span onClick={() => setIsModal(false)}>close</span>
-		</aside>
+		<>
+			{Open && (
+				<aside className='modal'>
+					<div className='con'>{children}</div>
+					<span onClick={() => setOpen(false)}>close</span>
+				</aside>
+			)}
+		</>
 	);
-};
+});
 
 export default Modal;
