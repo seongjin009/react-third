3f1663cfc7b0f80dd62c5ea2dffd38ed52df55b8
seongjin009 <iseongjin009@gmail.com>
2023-10-10 13:41:16 +0900

    로딩 시 이미지 표시

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -6,6 +6,10 @@ import Masonry from 'react-masonry-component';
 export default function Gallery(opt) {
 	const refInput = useRef(null);
 
+	const refFrame = useRef(null);
+
+	const [Loader, setLoader] = useState(false);
+
 	const [Pics, setPics] = useState([]);
 
 	const my_id = '199282986@N03';
@@ -37,6 +41,19 @@ export default function Gallery(opt) {
 			return alert('해당 검색어의 결과값이 없습니다');
 		}
 		setPics(json.photos.photo);
+
+		let count = 0;
+		const imgs = refFrame.current?.querySelectorAll('img');
+
+		imgs.forEach((img, idx) => {
+			img.onload = () => {
+				++count;
+				console.log('현재 로딩된 img갯수', count);
+				if (count === imgs.length) {
+					console.log('모든 이미지 소스 렌더링 완료!');
+				}
+			};
+		});
 	};
 
 	useEffect(() => {
@@ -50,61 +67,70 @@ export default function Gallery(opt) {
 
 	return (
 		<Layout title={'Gallery'}>
-			<form
-				onSubmit={(e) => {
-					//submit이벤트의 기본 서버 전송기능을 막아줌
-					e.preventDefault();
-					//문자열.trim() : 문자열앞뒤로 빈칸을 제거해서 정리
-					if (refInput.current.value.trim() === '') {
-						return alert('검색어를 입력하세요.');
-					}
-
-					fetchData({ type: 'search', tags: refInput.current.value });
-					refInput.current.value = '';
-				}}
-			>
-				<input ref={refInput} type='text' placeholder='검색어를 입력하세요' />
-				<button>검색</button>
-			</form>
-			<button onClick={() => fetchData({ type: 'user', id: my_id })}>mygallery</button>
-			<button onClick={() => fetchData({ type: 'interest' })}>interst</button>
-			<button onClick={() => fetchData({ type: 'search', tags: 'ocean' })}>search</button>
-			<div className='picFrame'>
-				<Masonry
-					className={'my-gallery-class'} // maysonry로 wapping후 세팅
-					elementType={'ul'} // masonry컴포넌트가 변환될 태그명 지정
-					options={{ transitionDuration: '0,5s' }} // 박스모션시 transition 시간 설정
-					disableImagesLoaded={false} //true이미지로딩 처리 안함
-					updateOnEachImageLoad={false} //true각 이미지의 로딩처리 안함
+			<div className='searchBox'>
+				<form
+					onSubmit={(e) => {
+						e.preventDefault();
+						if (refInput.current.value.trim() === '') {
+							return alert('검색어를 입력하세요.');
+						}
+						fetchData({ type: 'search', tags: refInput.current.value });
+						refInput.current.value = '';
+					}}
 				>
-					{Pics.map((data, idx) => {
-						return (
-							<article key={idx}>
-								<div className='inner'>
-									<img
-										className='pic'
-										src={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`}
-										alt={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_b.jpg`}
-									/>
-									<h2>{data.title}</h2>
-									<div className='profile'>
-										<img
-											src={`http://farm${data.farm}.staticflickr.com/${data.server}/buddyicons/${data.owner}.jpg`}
-											alt={data.owner}
-											onError={(e) => {
-												e.target.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif');
-											}}
-										/>
-										<span onClick={() => fetchData({ type: 'user', id: data.owner })}>
-											{data.owner}
-										</span>
-									</div>
-								</div>
-							</article>
-						);
-					})}
-				</Masonry>
+					<input ref={refInput} type='text' placeholder='검색어를 입력하세요' />
+					<button>검색</button>
+				</form>
 			</div>
+			<div className='btnSet'>
+				<button onClick={() => fetchData({ type: 'user', id: my_id })}>My Gallery</button>
+				<button onClick={() => fetchData({ type: 'interest' })}>Interest Gallery</button>
+			</div>
+			{/* Loader가 true: 로딩바출력, Loader가 false: 갤러리 프레임 출력 */}
+			{Loader ? (
+				<img className='loading' src={`${process.env.PUBLIC_URL}/img/loading.gif`} alt='loading' />
+			) : (
+				<div className='picFrame' ref={refFrame}>
+					<Masonry
+						elementType={'div'}
+						options={{ transitionDuration: '0.5s' }}
+						disableImagesLoaded={false}
+						updateOnEachImageLoad={false}
+					>
+						{Pics.map((data, idx) => {
+							return (
+								<article key={idx}>
+									<div className='inner'>
+										<img
+											className='pic'
+											src={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`}
+											alt={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_b.jpg`}
+										/>
+										<h2>{data.title}</h2>
+										<div className='profile'>
+											<img
+												src={`http://farm${data.farm}.staticflickr.com/${data.server}/buddyicons/${data.owner}.jpg`}
+												alt={data.owner}
+												onError={(e) => {
+													//만약 사용자가 프로필 이미지를 올리지 않았을때 엑박이 뜨므로
+													//onError이벤트를 연결해서 대체이미지 출력
+													e.target.setAttribute(
+														'src',
+														'https://www.flickr.com/images/buddyicon.gif'
+													);
+												}}
+											/>
+											<span onClick={() => fetchData({ type: 'user', id: data.owner })}>
+												{data.owner}
+											</span>
+										</div>
+									</div>
+								</article>
+							);
+						})}
+					</Masonry>
+				</div>
+			)}
 		</Layout>
 	);
 }
