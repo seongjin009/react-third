ee36d41ffdfd7838752f38e9b018e7c0d25b8222
seongjin009 <iseongjin009@gmail.com>
2023-10-13 09:17:12 +0900

    커뮤니티 페이지 만들기

--- src/components/sub/cummunity/Community.jsx
+++ src/components/sub/cummunity/Community.jsx
@@ -2,9 +2,5 @@ import Layout from '../../common/layout/Layout';
 import('./Community.scss');
 
 export default function Community() {
-	return (
-		<Layout>
-			<p>게시판페이지 입니다.</p>
-		</Layout>
-	);
+	return <Layout title={'Community'}></Layout>;
 }
