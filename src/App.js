da2a842276e3b5f255ba76d5fcc6d1287c75de32
seongjin009 <iseongjin009@gmail.com>
2023-10-13 15:38:24 +0900

    메인페이지 만들기

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Contact from './components/sub/contact/Contact';
 import Community from './components/sub/cummunity/Community';
 
 import Detail from './components/sub/youtube/Detail';
+import Main from './components/main/mainWrap/Main';
 
 function App() {
 	return (
@@ -17,7 +18,9 @@ function App() {
 			<Switch>
 				<Route exact path='/'>
 					{/* 메인페이지 전용 헤더 */}
+
 					<Header isMain={true} />
+					<Main />
 				</Route>
 				<Route path='/'>
 					{/* 서브페이지 전용 헤더 */}
---
+++ src/components/main/info/Info.jsx
@@ -0,0 +1,10 @@
+import './Info.scss';
+
+function Info() {
+	return (
+		<section className='info'>
+			<h2>Information</h2>
+		</section>
+	);
+}
+export default Info;
