67b9391ec1062bd06ab071ad74374ad7e8632f37
seongjin009 <iseongjin009@gmail.com>
2023-10-16 15:28:24 +0900

    초기화 버튼 클릭시 모두 다 초기화

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,25 +1,7 @@
 import './Visual.scss';
 
 function Visual() {
-	return (
-		<section className='visual'>
-			<main>
-				<img className='backimg' src='./././img/mainBack.jpeg' />
-				<h1 className='title'>
-					My Portfolio
-					<div className='line1'></div>
-					<span>Lorem ipsum dolor sit amet.</span>
-				</h1>
-				<div className='director'>
-					<h2>Design By Seongjin</h2>
-				</div>
-				<div className='bar'>
-					<p>2023</p>
-					<div className='line'></div>
-				</div>
-			</main>
-		</section>
-	);
+	return <section className='visual'></section>;
 }
 
 export default Visual;
--- src/components/sub/members/Members.jsx
+++ src/components/sub/members/Members.jsx
@@ -1,82 +1,105 @@
 import Layout from '../../common/layout/Layout';
 import './Members.scss';
-import { useState } from 'react';
-
+import { useState, useRef } from 'react';
 export default function Members() {
 	const initVal = {
 		userid: '',
 		pwd1: '',
 		pwd2: '',
 		email: '',
+		gender: false,
+		interests: false,
+		edu: '',
+		comments: '',
 	};
-	const [Errs, setErrs] = useState({});
-
 	const [Val, setVal] = useState(initVal);
+	const [Errs, setErrs] = useState({});
+	const refCheckGroup = useRef(null);
+	const refRadioGroup = useRef(null);
+	const refschool = useRef(null);
 
+	console.log(Errs);
 	const resetForm = (e) => {
 		e.preventDefault();
 		setVal(initVal);
+		[refCheckGroup, refRadioGroup].forEach((el) =>
+			el.current.querySelectorAll('input').forEach((input) => (input.checked = false))
+		);
+		refschool.current.value = '';
 	};
 
 	const handleChange = (e) => {
 		const { name, value } = e.target;
-		//현재 onChange이벤트가 발생하고 있는 form요소의 name값을 객체안에서 변수로 가져오소 value값도 가져온뒤 기존의 state값을 deep copy하고있는 input의 property값만 덮어쓰기
 		setVal({ ...Val, [name]: value });
 	};
-
 	const handleRadio = (e) => {
 		const { name, checked } = e.target;
 		setVal({ ...Val, [name]: checked });
 	};
-
-	//인수값으로 state를 전달받아서 각 데이터별로 인증처리 후
-	//만약 인증에러가 발생하면 해당 name값으로 에러문구를 생성해서 반환하는 함수
+	const handleCheck = (e) => {
+		const { name } = e.target;
+		let isChecked = false;
+		const inputs = e.target.parentElement.querySelectorAll('input');
+		inputs.forEach((input) => input.checked && (isChecked = true));
+		setVal({ ...Val, [name]: isChecked });
+	};
 	const check = (value) => {
-		const num = /[0-9]/; //0-9까지의 모든 값을 정규표현식으로 범위 지정
-		const txt = /[a-zA-Z]/; //대소문자 구분없이 모든문자 범위 지정
-		const spc = /[!@#$%^&*()_]/; //모든 특수문자 지정
+		const num = /[0-9]/; //0-9까지의 모든 값을 정규표현식으로 범위지정
+		const txt = /[a-zA-Z]/; //대소문자 구분없이 모든 문자 범위지정
+		const spc = /[!@#$%^*()_]/; //모든 특수문자 지정
 		const errs = {};
-
 		if (value.userid.length < 5) {
 			errs.userid = '아이디는 최소 5글자 이상 입력하세요.';
 		}
-
-		//비밀번호 인증 (5글자 이상, 문자, 숫자, 특수문ㅁ자 모두 포함)
+		//비밀번호 인증 (5글자 이상, 문자, 숫자, 특수문자 모두 포함)
 		if (
 			value.pwd1.length < 5 ||
 			!num.test(value.pwd1) ||
 			!txt.test(value.pwd1) ||
 			!spc.test(value.pwd1)
 		) {
-			errs.pwd1 = '비밀번호는 5글자이상, 문자, 숫자, 특수문자를 모두 포함하세요';
+			errs.pwd1 = '비밀번호는 5글자이상, 문자,숫자,특수문자를 모두 포함하세요';
 		}
-
-		//2차 비밀번호 인증
+		//비밀번호 재확인 인증
 		if (value.pwd1 !== value.pwd2 || !value.pwd2) {
 			errs.pwd2 = '2개의 비밀번호를 같게 입력하세요.';
 		}
-
 		//이메일 인증
 		if (!value.email || !/@/.test(value.email)) {
 			errs.email = '이메일은 무조건 @를 포함해야 합니다.';
 		} else {
-			if (!value.email.split('@')[0] || !value.email.split('@')[1]) {
+			const [forward, backward] = value.email.split('@');
+			if (!forward || !backward) {
 				errs.email = '이메일에 @앞뒤로 문자값이 있어야 합니다.';
 			} else {
-				if (!value.email.split('@')[1].split('.')[0] || !value.email.split('@')[1].split('.')[1]) {
+				const [forward, backward] = value.email.split('.');
+				if (!forward || !backward) {
 					errs.email = '이메일 . 앞뒤로 문자값이 있어야 합니다.';
 				}
 			}
 		}
 		//성별인증
 		if (!value.gender) {
-			errs.gender = '성별을 하나이상 체크해주세요';
+			errs.gender = '성별은 필수 체크항목입니다.';
+		}
+		//관심사인증
+		if (!value.interests) {
+			errs.interests = '관심사를 하나이상 체크해주세요.';
+		}
+		//학력 인증
+		if (!value.edu) {
+			errs.edu = '학력을 선택하세요.';
+		}
+		//남기는말 인증
+		if (value.comments.length < 10) {
+			errs.comments = '남기는말은 10글자 이상 입력하세요.';
 		}
 		return errs;
 	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
+
 		if (Object.keys(check(Val)).length === 0) {
 			alert('인증통과');
 		} else {
@@ -88,9 +111,9 @@ export default function Members() {
 			<form onSubmit={handleSubmit}>
 				<fieldset>
 					<legend className='h'>회원가입 폼 양식</legend>
-					<table border={1}>
+					<table border='1'>
 						<tbody>
-							{/*userid */}
+							{/* userid */}
 							<tr>
 								<th scope='row'>
 									<label htmlFor='userid'>userid</label>
@@ -100,15 +123,13 @@ export default function Members() {
 										type='text'
 										id='userid'
 										name='userid'
-										//onChange가 발생할때마다 실시간으로 변경되고 있는 value값만 출력
 										value={Val.userid}
 										onChange={handleChange}
 									/>
 									{Errs.userid && <p>{Errs.userid}</p>}
 								</td>
 							</tr>
-
-							{/*password */}
+							{/* password */}
 							<tr>
 								<th scope='row'>
 									<label htmlFor='pwd1'>password</label>
@@ -124,15 +145,14 @@ export default function Members() {
 									{Errs.pwd1 && <p>{Errs.pwd1}</p>}
 								</td>
 							</tr>
-
-							{/*re_password */}
+							{/* re password */}
 							<tr>
 								<th scope='row'>
-									<label htmlFor='pwd2'>re_password</label>
+									<label htmlFor='pwd2'>re-password</label>
 								</th>
 								<td>
 									<input
-										type='text'
+										type='password'
 										id='pwd2'
 										name='pwd2'
 										value={Val.pwd2}
@@ -141,7 +161,7 @@ export default function Members() {
 									{Errs.pwd2 && <p>{Errs.pwd2}</p>}
 								</td>
 							</tr>
-							{/*email */}
+							{/* email */}
 							<tr>
 								<th scope='row'>
 									<label htmlFor='email'>e-mail</label>
@@ -157,23 +177,68 @@ export default function Members() {
 									{Errs.email && <p>{Errs.email}</p>}
 								</td>
 							</tr>
-
+							{/* gender */}
 							<tr>
 								<th>gender</th>
-								<td>
+								<td ref={refRadioGroup}>
 									<label htmlFor='female'>female</label>
 									<input type='radio' name='gender' id='female' onChange={handleRadio} />
-									<label htmlFor='female'>male</label>
+									<label htmlFor='male'>male</label>
 									<input type='radio' name='gender' id='male' onChange={handleRadio} />
 									{Errs.gender && <p>{Errs.gender}</p>}
 								</td>
 							</tr>
-
-							{/*btnSet */}
+							{/* interests */}
+							<tr>
+								<th>interests</th>
+								<td ref={refCheckGroup}>
+									<label htmlFor='sports'>sports</label>
+									<input type='checkbox' id='sports' name='interests' onChange={handleCheck} />
+									<label htmlFor='game'>game</label>
+									<input type='checkbox' id='game' name='interests' onChange={handleCheck} />
+									<label htmlFor='music'>music</label>
+									<input type='checkbox' id='music' name='interests' onChange={handleCheck} />
+									{Errs.interests && <p>{Errs.interests}</p>}
+								</td>
+							</tr>
+							{/* education */}
+							<tr>
+								<th>
+									<label htmlFor='edu'>Education</label>
+								</th>
+								<td>
+									<select name='edu' id='edu' onChange={handleChange} ref={refschool}>
+										<option value=''>최종학력 선택하세요</option>
+										<option value='elementary-school'>초등학교 졸업</option>
+										<option value='middle-school'>중학교 졸업</option>
+										<option value='high-school'>고등학교 졸업</option>
+										<option value='college'>대학교 졸업</option>
+									</select>
+									{Errs.edu && <p>{Errs.edu}</p>}
+								</td>
+							</tr>
+							{/* comments */}
+							<tr>
+								<th>
+									<label htmlFor='comments'>comments</label>
+								</th>
+								<td>
+									<textarea
+										name='comments'
+										id=''
+										cols='30'
+										rows='3'
+										value={Val.comments}
+										onChange={handleChange}
+									></textarea>
+									{Errs.comments && <p>{Errs.comments}</p>}
+								</td>
+							</tr>
+							{/* btnSet */}
 							<tr>
 								<th colSpan='2'>
-									<input type='reset' defaultValue='cancel' onClick={resetForm} />
-									<input type='submit' defaultValue='send' onClick={resetForm} />
+									<input type='reset' value='cancel' onClick={resetForm} />
+									<input type='submit' value='send' onClick={resetForm} />
 								</th>
 							</tr>
 						</tbody>
