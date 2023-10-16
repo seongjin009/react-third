b8efffa14b7d8a077283110f7c126bf6d35db00e
seongjin009 <iseongjin009@gmail.com>
2023-10-16 09:27:13 +0900

    인페이지 처음 접속시 로컬저장소에 값이 없어서 오류가 뜨는 문제 해결

--- src/components/main/news/News.jsx
+++ src/components/main/news/News.jsx
@@ -2,11 +2,14 @@ import './News.scss';
 import { useState } from 'react';
 
 function News() {
+	//프로젝트가 처음 구동되면 무조건 메인페이지에 갔다가 커뮤니티페이지에 넘어가는 구조
+	//해당 페이지에 있는 함수가 처음 구동될 시로컬저장소에 값이 없으므로
+	//저장소값이 없을 때 빈배열이 반환되는 구문을 추가 (중요)
 	const getLocalData = () => {
 		const data = localStorage.getItem('post');
 		return JSON.parse(data);
 	};
-	const [Post, setPost] = useState(getLocalData());
+	const [Post] = useState(getLocalData());
 
 	return (
 		<section className='news'>
