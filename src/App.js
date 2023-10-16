a7e954546d8cb6e53b8084cd624756c48ac00010
seongjin009 <iseongjin009@gmail.com>
2023-10-16 16:24:43 +0900

    커뮤니티 페이지 스타일 변경

--- src/App.js
+++ src/App.js
@@ -10,10 +10,13 @@ import Community from './components/sub/cummunity/Community';
 
 import Detail from './components/sub/youtube/Detail';
 import Main from './components/main/mainWrap/Main';
+import { useRef } from 'react';
 
 function App() {
+	const refMain = useRef(null);
+
 	return (
-		<>
+		<main ref={refMain}>
 			{/* Switch안쪽에서 중첩되는 조건 라우트의 컴포넌트가 있을때 위쪽의 조건의 컴포넌트만 호출하고 나머지 무시 */}
 			<Switch>
 				<Route exact path='/'>
@@ -36,7 +39,7 @@ function App() {
 			<Route path='/community' component={Community} />
 
 			{/* params는 url에 특정 컴포넌트를 연결할 때 url로 정보값을 같이 전달 경로/:변수명 */}
-		</>
+		</main>
 	);
 }
 
