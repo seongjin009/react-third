7c011db31e04c03cf7680e95888fcc5145c17c22
seongjin009 <iseongjin009@gmail.com>
2023-10-17 16:11:15 +0900

    오류해결

--- src/redux/youtubeSlice.js
+++ src/redux/youtubeSlice.js
@@ -1,32 +1,28 @@
 //비동기 데이터 frtching함수 등록
 //비동기 데이터의 상태에 따라서 자동으로 해당 데이터를 변경할 수 있는 리듀서라는 함수를 등록
-
 import axios from 'axios';
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-
-//비동기 서버통신으로 데이터를 전달받아서 내부적으로 action차입을 자동으로 생성해서 액션개체 생성까지 완료하는 함수
+//비동기 서버통신으로 데이터를 전달받아서 내부적으로 action타입을 자동 생성해서 액션객체 생성까지 완료하는 함수
 export const fetchYoutube = createAsyncThunk('youtube/request', async () => {
 	const api_key = process.env.REACT_APP_YOUTUBE_API;
 	const baseURL = 'https://www.googleapis.com/youtube/v3/playlistItems';
-	const pid = `PLJ0_dUpwgnHGNKFN5G2r6rsMf51JmkB7M`;
-	const num = 1;
-	const resultURL = `${baseURL}?key=${api_key}&part=snippet&playlistId=${pid}&maxResult=${num}`;
-
+	const pid = 'PLHtvRFLN5v-W5bQjvyH8QTdQQhgflJ3nu';
+	const num = 5;
+	const resultURL = `${baseURL}?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
 	const result = await axios.get(resultURL);
 	return result.data.items;
 });
-
-//{type:'대기}
-//{type:'성공' : payload:[데이터]}
-//{type:'실패' : payload :에러객체}
-
-//createAsyncThunk가 반환하는 acrion객체를 받아서 전역스토어 데이터를 변현하는 reducer 함수등록
+//{type:'대기'}
+//{type:'성공': payload:[데이터]}
+//{type:'실패': payload:에러객체}
+//createAsyncThunk가 반환하는 action객체를 받아서 전역스토어 데이터를 변형하는 reducer함수등록
 const youtubeSlice = createSlice({
 	name: 'youtube',
-	initalState: {
-		date: [],
+	initialState: {
+		data: [],
+		isLoading: false,
 	},
-	extraReducer: {
+	extraReducers: {
 		[fetchYoutube.pending]: (state) => {
 			state.isLoading = true;
 		},
@@ -40,7 +36,6 @@ const youtubeSlice = createSlice({
 		},
 	},
 });
-
 export default youtubeSlice.reducer;
 
 //리덕스에서 전역상태 관리에 쓰이는 용어정리
