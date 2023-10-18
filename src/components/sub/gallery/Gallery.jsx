00c2a92b1799c282bc7e8ffc0c8ec3a6a44b8334
seongjin009 <iseongjin009@gmail.com>
2023-10-18 10:48:22 +0900

    모달관련 client slide 패키지 제작

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,94 +1,61 @@
 import Layout from '../../common/layout/Layout';
-import './Gallery.scss';
-import { useState, useEffect, useRef } from 'react';
-import Masonry from 'react-masonry-component';
 import Modal from '../../common/modal/Modal';
-
-export default function Gallery(opt) {
+import './Gallery.scss';
+import { useState, useRef } from 'react';
+import Masonry from 'react-masonry-component';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchFlickr } from '../../../redux/flickrslice';
+export default function Gallery() {
+	const dispatch = useDispatch();
+	const Pics = useSelector((store) => store.flickr.data);
 	const refInput = useRef(null);
-
-	const [Pics, setPics] = useState([]);
-
-	const my_id = '199282986@N03';
-
-	const refbtnSet = useRef(null);
-
+	const refBtnSet = useRef(null);
 	const [ActiveURL, setActiveURL] = useState('');
-
-	const [IsModal, setIsModal] = useState(false);
-
 	const [IsUser, setIsUser] = useState(true);
-
-	const fetchData = async (opt) => {
-		let url = '';
-		const api_key = '3d01d56ea3c92153f235a2985a9776f6';
-		const method_interest = 'flickr.interestingness.getList';
-		const method_user = 'flickr.people.getPhotos';
-		const method_search = 'flickr.photos.search';
-		const num = 50;
-
-		if (opt.type === 'interest') {
-			url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json`;
-		}
-		if (opt.type === 'user') {
-			url = `https://www.flickr.com/services/rest/?method=${method_user}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json&user_id=${opt.id}`;
-		}
-		if (opt.type === 'search') {
-			url = `https://www.flickr.com/services/rest/?method=${method_search}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json&tags=${opt.tags}`;
-		}
-
-		const data = await fetch(url);
-		const json = await data.json();
-		console.log(json.photos.photo);
-		if (json.photos.photo.length === 0) {
-			return alert('해당 검색어의 결과값이 없습니다');
-		}
-		setPics(json.photos.photo);
-	};
-
-	//submit 이벤트 발생시 실행할 함수
+	const [IsModal, setIsModal] = useState(false);
+	const my_id = '199282986@N03';
 	const handleSubmit = (e) => {
 		e.preventDefault();
-
 		setIsUser(false);
-		const btns = refbtnSet.current.querySelectorAll('button');
+		const btns = refBtnSet.current.querySelectorAll('button');
 		btns.forEach((btn) => btn.classList.remove('on'));
-
 		if (refInput.current.value.trim() === '') {
 			return alert('검색어를 입력하세요.');
 		}
-		fetchData({ type: 'search', tags: refInput.current.value });
+
+		dispatch(fetchFlickr({ type: 'search', tags: refInput.current.value }));
 		refInput.current.value = '';
 	};
 
+	//myGallery 클릭 이벤트 발생시 실행할 함수
 	const handleClickMy = (e) => {
 		setIsUser(true);
 		if (e.target.classList.contains('on')) return;
-		const btns = refbtnSet.current.querySelectorAll('button');
+		const btns = refBtnSet.current.querySelectorAll('button');
 		btns.forEach((btn) => btn.classList.remove('on'));
 		e.target.classList.add('on');
-		fetchData({ type: 'user', id: my_id });
+
+		dispatch(fetchFlickr({ type: 'user', id: my_id }));
 	};
 
+	//Interest Gallery 클릭 이벤트 발생시 실행할 함수
 	const handleClickInterest = (e) => {
 		setIsUser(false);
-
 		if (e.target.classList.contains('on')) return;
-		const btns = refbtnSet.current.querySelectorAll('button');
+		const btns = refBtnSet.current.querySelectorAll('button');
 		btns.forEach((btn) => btn.classList.remove('on'));
 		e.target.classList.add('on');
-		fetchData({ type: 'interest' });
+
+		dispatch(fetchFlickr({ type: 'interest' }));
 	};
 
+	//profile 아이디 클릭시 실행할 함수
 	const handleClickProfile = (e) => {
 		if (IsUser) return;
-		fetchData({ type: 'user', id: e.target.innerText });
+
+		dispatch(fetchFlickr({ type: 'user', id: e.target.innerText }));
 		setIsUser(true);
 	};
-
-	useEffect(() => {
-		fetchData({ type: 'search', tags: 'house' });
-	}, []);
 
 	return (
 		<>
@@ -100,7 +67,7 @@ export default function Gallery(opt) {
 					</form>
 				</div>
 
-				<div className='btnSet' ref={refbtnSet}>
+				<div className='btnSet' ref={refInput}>
 					<button onClick={handleClickMy}>My Gallery</button>
 
 					<button className='on' onClick={handleClickInterest}>
