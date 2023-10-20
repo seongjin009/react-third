a3fc8106347f9b91448ce96a82f2074cbd48b4d0
seongjin009 <iseongjin009@gmail.com>
2023-10-20 16:40:06 +0900

    메인페이지 flickr 데이터 쿼리 방식으로 호출

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
-import flickrReducer from './redux/flickrslice';
 import modalReducer from './redux/modalSlick';
 import menuReducer from './redux/menuSlice';
 
@@ -12,7 +11,6 @@ import menuReducer from './redux/menuSlice';
 //store에는 하나의 객체만 등록가능하기 때문에 여러개의 데이터 카테고리는 reducer로 통합해서 등록
 const store = configureStore({
 	reducer: {
-		flickr: flickrReducer,
 		modal: modalReducer,
 		menu: menuReducer,
 	},
