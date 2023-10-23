c37402bbeb33cd6773743fc55363dc92f39eca85
seongjin009 <iseongjin009@gmail.com>
2023-10-23 10:25:16 +0900

    contact 페이지 변경

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -4,21 +4,16 @@ import './Contact.scss';
 import { useRef, useEffect, useState, useCallback } from 'react';
 
 export default function Contact() {
-	const form = useRef(null);
 	const map = useRef(null);
 	const view = useRef(null);
 	const instance = useRef(null);
+	const form = useRef(null);
 	const [Traffic, setTraffic] = useState(false);
 	const [Index, setIndex] = useState(0);
 	const [IsMap, setIsMap] = useState(true);
 
-	//kakao api를 cdn방식으로 불러오고 있기 때문에 리액트 컴포넌트가 실행되면 window객체에서 직접 비구조화 할당으로 kakao객체를 뽑아옴
 	const { kakao } = window;
-	//첫번째 지도를 출력하기 위한 객체정보
 
-	//지도정보데이터를 객체형식으로 구조화한 다음에 데이터 기반으로 자동 지도화면이 생성되도록 만들었다.
-	//데이터정보가 많아질때를 대비해서 유지보수에 최적화되도록 코드 개선
-	//해당 정보값은 자주 바뀌는값이 아니기 때문에 굳이 state에 담아서 불필요한 재랜더링을 막기위해 useRef에 담아놨다
 	const info = useRef([
 		{
 			title: '삼성역 코엑스',
@@ -43,67 +38,46 @@ export default function Contact() {
 		},
 	]);
 
-	//위의 정보값을 활용한 마커 객체 생성
-	const marker = new kakao.maps.Marker({
-		position: info.current[Index].latlng,
-		image: new kakao.maps.MarkerImage(
-			info.current[Index].imgSrc,
-			info.current[Index].imgSize,
-			info.current[Index].imgPos
-		),
-	});
-
-	//지도위치를 중심으로 이동시키는 핸들러 함수 제작
-	const setCenter = () => {
-		console.log('지도화면에서 마커 가운데 보정');
-		// 지도 중심을 이동 시킵니다
+	const setCenter = useCallback(() => {
 		instance.current.setCenter(info.current[Index].latlng);
-	};
+	}, [Index]);
 
 	useEffect(() => {
-		//Index값이 변경될때마다 새로운 지도 레이어가 중첩되므로
-		//일단은 기존 map안의 모든 요소를 없애서 초기화
+		const marker = new kakao.maps.Marker({
+			position: info.current[Index].latlng,
+			image: new kakao.maps.MarkerImage(
+				info.current[Index].imgSrc,
+				info.current[Index].imgSize,
+				info.current[Index].imgPos
+			),
+		});
+
 		map.current.innerHTML = '';
-		//객체 정보를 활용한 지도 객체 생성
+
 		instance.current = new kakao.maps.Map(map.current, {
 			center: info.current[Index].latlng,
 			level: 1,
 		});
-		//마커 객체에 지도 객체 연결
+
 		marker.setMap(instance.current);
 
-		//지도 타입 변경 UI추가
 		const mapTypeControl = new kakao.maps.MapTypeControl();
 		instance.current.addControl(mapTypeControl, kakao.maps.ControlPosition.BOTTOMLEFT);
 
-		//지도 생성시 마커 고정적으로 적용되기 때문에 브라우저 리사이즈시 마커가 가운데 위치하지 않는 문제
-		//마커를 가운데 고정시키는 함수를 제작한뒤 윈도우객체 직접 resize이벤트 발생시마다 핸들러함수 호출해서 마커위치 보정
-
-		//Contact페이지에만 동작되야 되는 핸들러함수를 최상위 객체인 window에 직접 연결했기 때문에
-		//라우터로 다른페이지이동하더라도 계속해서 setCenter호출되는 문제점 발생
-		//해결방법: Contact 컴포넌트가 언마운트시 강제로 윈도우객체에서 setCenter핸들러를 제거
 		window.addEventListener('resize', setCenter);
 
-		//로드뷰 관련 코드
-		new kakao.maps.RoadviewClient().getNearestPanoId(
-			info.current[Index].latlng,
-			100, //해당 지도의 위치값에서 반경 100미터 안에 제일 가까운 도로 기준으로 로드뷰화면 생성
-			(panoId) => {
-				new kakao.maps.Roadview(view.current).setPanoId(panoId, info.current[Index].latlng);
-			}
-		);
+		new kakao.maps.RoadviewClient().getNearestPanoId(info.current[Index].latlng, 50, (panoId) => {
+			new kakao.maps.Roadview(view.current).setPanoId(panoId, info.current[Index].latlng);
+		});
 
-		return () => {
-			window.removeEventListener('resize', setCenter);
-		};
-	}, [Index, kakao]); //Index값이 변경될때마다 지도화면이 다시 갱신되어야 하므로 Index값을 의존성 배열에 등록
+		return () => window.removeEventListener('resize', setCenter);
+	}, [Index, kakao, setCenter]);
 
 	useEffect(() => {
-		//traffic 값이 바뀔때마다 실행될 구문
 		Traffic
 			? instance.current.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
 			: instance.current.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
-	}, [Traffic]);
+	}, [Traffic, kakao]);
 
 	const resetForm = () => {
 		const nameForm = form.current.querySelector('.nameEl');
@@ -114,7 +88,6 @@ export default function Contact() {
 		msgForm.value = '';
 	};
 
-	//form mail 기능함수
 	const sendEmail = (e) => {
 		e.preventDefault();
 
@@ -123,23 +96,27 @@ export default function Contact() {
 		const msgForm = form.current.querySelector('.msgEl');
 
 		if (!nameForm.value || !mailForm.value || !msgForm.value)
-			return alert('사용자이름, 이메일주소, 문의내용은 필수 입력사항입니다.');
+			return alert('사용자이름,이메일주소,문의내용은 필수 입력사항입니다');
 
-		//sendForm메서드는 각 키값을 문자열로만 인수로 전달되도록 type지정되어 있기 때문에
-		//변수를 `${}`로 감싸서 문자형식으로 전달
-
-		emailjs.sendForm(`service_e9crmle`, `template_ykzkm22`, form.current, `rK8Krscf90c8RrFsS`).then(
-			(result) => {
-				alert('문의내용이 메일로 발송되었습니다.');
-				console.log(result);
-				resetForm();
-			},
-			(error) => {
-				alert('문의내용 전송에 실패했습니다.');
-				console.log(error);
-				resetForm();
-			}
-		);
+		emailjs
+			.sendForm(
+				`${process.env.REACT_APP_SERVICE_ID}`,
+				`${process.env.REACT_APP_TEMPLATE_ID}`,
+				form.current,
+				`${process.env.REACT_APP_PUBLIC_KEY}`
+			)
+			.then(
+				(result) => {
+					alert('문의내용이 메일로 발송되었습니다.');
+					console.log(result);
+					resetForm();
+				},
+				(error) => {
+					alert('문의내용 전송에 실패했습니다.');
+					console.log(error);
+					resetForm();
+				}
+			);
 	};
 
 	return (
