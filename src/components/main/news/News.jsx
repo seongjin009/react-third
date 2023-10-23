e0f3c6b7921420c33f1fe1a8b40a036eb72f8858
seongjin009 <iseongjin009@gmail.com>
2023-10-23 09:24:37 +0900

    news페이지 의존성 배열 처리

--- src/components/main/news/News.jsx
+++ src/components/main/news/News.jsx
@@ -1,27 +1,45 @@
 import './News.scss';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback, useMemo } from 'react';
 
 function News() {
-	const dyndata1 = [
-		{
-			title: 'jeonghyn',
-			content: "hello jeonghyun i'm seongjin",
-			data: new Date(),
-		},
-	];
+	const dyndata1 = useMemo(() => {
+		return [
+			{
+				title: 'jeonghyn',
+				content: "hello jeonghyun i'm seongjin",
+				data: new Date(),
+			},
+			{
+				title: 'jeonghyn',
+				content: "hello jeonghyun i'm seongjin",
+				data: new Date(),
+			},
+			{
+				title: 'jeonghyn',
+				content: "hello jeonghyun i'm seongjin",
+				data: new Date(),
+			},
+			{
+				title: 'jeonghyn',
+				content: "hello jeonghyun i'm seongjin",
+				data: new Date(),
+			},
+		];
+	}, []);
 	//프로젝트가 처음 구동되면 무조건 메인페이지에 갔다가 커뮤니티페이지에 넘어가는 구조
 	//해당 페이지에 있는 함수가 처음 구동될 시로컬저장소에 값이 없으므로
 	//저장소값이 없을 때 빈배열이 반환되는 구문을 추가 (중요)
-	const getLocalData = () => {
+	const getLocalData = useCallback(() => {
 		const data = localStorage.getItem('post');
 		if (data) return JSON.parse(data);
 		else return dyndata1;
-	};
+	}, [dyndata1]);
+
 	const [Post, setPost] = useState(getLocalData());
 
 	useEffect(() => {
 		setPost(getLocalData());
-	}, []);
+	}, [getLocalData]);
 
 	return (
 		<section className='news myScroll'>
